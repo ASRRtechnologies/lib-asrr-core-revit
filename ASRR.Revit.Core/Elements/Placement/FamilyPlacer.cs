6259367d832576d7ebdcae0159cfe0bf4469679c
Amar <armd@axi.nl>
2022-04-30 21:32:32 +0200

    Clean up

---
+++ ASRR.Revit.Core/Elements/Placement/FamilyPlacer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+
+namespace ASRR.Core.Revit.Elements.Placement
+{
+    public class FamilyPlacer
+    {
+
+
+
+        public static IEnumerable<FamilyInstance> GetFamilyInstancesByFamilyName(Document doc, string familyName)
+        {
+            try
+            {
+                IEnumerable<FamilyInstance> familyInstances = new FilteredElementCollector(doc)
+                    .OfClass(typeof(FamilyInstance))
+                    .Cast<FamilyInstance>()
+                    .Where(x => x.Symbol.Family.Name.Contains(familyName));
+                return familyInstances;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static IEnumerable<FamilyInstance> GetFamilyInstancesByFamilyAndType(Document doc, string familyName, string typeName)
+        {
+            try
+            {
+                return new FilteredElementCollector(doc)
+                    .OfClass(typeof(FamilyInstance))
+                    .Cast<FamilyInstance>()
+                    .Where(x => x.Symbol.Family.Name.Equals(familyName)) // family
+                    .Where(x => x.Name.Equals(typeName)); // family type         
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ASRR.Revit.Core/Elements/Rotation/ElementRotator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace ASRR.Core.Revit.Elements.Rotation
+{
+    public class ElementRotator
+    {
+        public static bool RotateElement(Element element, double rotation)
+        {
+            bool rotated = false;
+            // Rotate the element via its location curve.
+            //LocationCurve curve = element.Location as LocationCurve;
+            //logger.Info($"curve : '{curve}'");
+            // LocationPoint elementPoint = element.Location as LocationPoint;
+            // logger.Info($"elementPoint : x:'{MmToFeet(elementPoint.Point.X)}', y:'{MmToFeet(elementPoint.Point.Y)}', z:'{MmToFeet(elementPoint.Point.Z)}'");
+            // if (null != elementPoint)
+            // {
+            //     //Curve line = elementPoint.Point;
+            //     XYZ aa = elementPoint.Point;
+            //     logger.Info($"aa : x:'{MmToFeet(aa.X)}', y:'{MmToFeet(aa.Y)}', z:'{MmToFeet(aa.Z)}'");
+            //     XYZ cc = new XYZ(aa.X, aa.Y, aa.Z + 10);
+            //     logger.Info($"cc : x:'{MmToFeet(cc.X)}', y:'{MmToFeet(cc.Y)}', z:'{MmToFeet(cc.Z)}'");
+            //     Line axis = Line.CreateBound(aa, cc);
+            //     rotated = elementPoint.Rotate(axis, rotation);
+            // }
+            // else
+            // {
+            //     logger.Info("elementPoint is null");
+            // }
+            // logger.Info($"rotated : '{rotated}'");
+            return rotated;
+        }
+    }
+}
