ccc701ce9edcd085a3f408502dbefdaadfef874d
Amar Ramdas <amar.ramdas@asrr.nl>
2022-05-01 18:07:01 +0200

    Update familyplacer to work with symbols

--- ASRR.Revit.Core/Elements/Placement/FamilyPlacer.cs
+++ ASRR.Revit.Core/Elements/Placement/FamilyPlacer.cs
@@ -18,14 +18,45 @@ namespace ASRR.Core.Revit.Elements.Placement
         /// This method places a family based on reference plane, location and referenceDirection
         /// </summary>
         public void Place(Document doc, 
+            string familyName,
+            string type,
             Reference reference,
             XYZ location,
-            XYZ referenceDirection,
-            FamilySymbol symbol)
+            XYZ referenceDirection)
         {
-            Log.Info($"Creating new placing instance at {location}");
-            // TODO: make this work based on family name + family type
+            var symbols = GetFamilySymbol(doc, familyName, type);
+
+            if (!symbols.Any())
+            {
+                Log.Error($"No matching family symbol for familyName '{familyName}' and type '{type}' ");
+                return;
+            }
+
+            if (symbols.Count() > 1)
+            {
+                Log.Warn("Found more than one matching symbol, using first symbol found.");
+            }
+
+            var symbol = symbols.First();
+            Log.Info($"Creating new placing instance at {location} with direction {referenceDirection}");
             doc.Create.NewFamilyInstance(reference, location, referenceDirection, symbol);
+        }
+
+        public List<FamilySymbol> GetFamilySymbol(Document doc, string familyName, string typeName)
+        {
+            try
+            {
+                return new FilteredElementCollector(doc)
+                    .OfClass(typeof(FamilySymbol))
+                    .Cast<FamilySymbol>()
+                    .Where(x => x.Family.Name.Equals(familyName)) // family
+                    .Where(x => x.Name.Equals(typeName))
+                    .ToList(); // family type         
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
         
         public IEnumerable<FamilyInstance> GetFamilyInstancesByFamilyName(Document doc, string familyName)
