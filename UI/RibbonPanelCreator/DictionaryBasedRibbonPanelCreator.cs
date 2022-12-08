00041c1b500d34b05ebcfc3a52e52fcd78cd0b00
Amar Ramdas <amar.ramdas@asrr.nl>
2022-12-08 17:05:19 +0100

    Update submodule

---
+++ UI/RibbonPanelCreator/DictionaryBasedRibbonPanelCreator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using ASRR.Revit.Core.UI.RibbonPanelItemCreator;
+using Autodesk.Revit.UI;
+
+namespace DigiBase.Modelleerversneller.Revit.AddIn.RibbonPanelCreator
+{
+    public class DictionaryBasedRibbonPanelsCreator : IRibbonPanelCreator
+    {
+        private readonly Dictionary<string, List<IRibbonPanelItemCreator>> _itemCreatorsPerPanelName;
+
+        public DictionaryBasedRibbonPanelsCreator(
+            Dictionary<string, List<IRibbonPanelItemCreator>> itemCreatorsPerPanelName)
+        {
+            _itemCreatorsPerPanelName = itemCreatorsPerPanelName;
+        }
+
+        public IEnumerable<RibbonPanel> Create(UIControlledApplication application, string tabName)
+        {
+            // initialize the result
+            var result = new List<RibbonPanel>();
+
+            // iterate all items, create the panels and the items for each panel
+            foreach (var panelName in _itemCreatorsPerPanelName.Keys)
+            {
+                var panel = application.CreateRibbonPanel(tabName, panelName);
+                result.Add(panel);
+
+                var panelItemCreators = _itemCreatorsPerPanelName[panelName];
+
+                foreach (var panelItemCreator in panelItemCreators) panelItemCreator.Create(panel);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ UI/RibbonPanelItemCreator/PushButtonRibbonPanelItemCreator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using Autodesk.Revit.UI;
+
+namespace ASRR.Revit.Core.UI.RibbonPanelItemCreator
+{
+    public class PushButtonRibbonPanelItemCreator<T> : IRibbonPanelItemCreator where T : IExternalCommand
+    {
+        private readonly string _title;
+        private readonly ImageSource _imageSource;
+
+        public PushButtonRibbonPanelItemCreator(string title, ImageSource imageSource)
+        {
+            _title = title;
+            _imageSource = imageSource;
+        }
+
+        public void Create(RibbonPanel panel)
+        {
+            //Get the type and assembly for the command
+            var commandType = typeof(T);
+            var commandName = commandType.FullName;
+
+            var commandAssembly = Assembly.GetAssembly(commandType);
+            var commandAssemblyName = commandAssembly.CodeBase;
+            var uri = new UriBuilder(commandAssemblyName);
+            var path = Uri.UnescapeDataString(uri.Path);
+
+            //Create PushButtonData first
+            var pushButtonData = new PushButtonData(_title, _title, path, commandName);
+
+            //Now add the button
+            var pushButton = panel.AddItem(pushButtonData) as PushButton;
+            pushButton.LargeImage = _imageSource;
+        }
+    }
+}
