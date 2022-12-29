749f6ef29ef35d569e34684e87d89852078388d1
Amar Ramdas <amar.ramdas@asrr.nl>
2022-12-29 15:26:54 +0100

    Update with png

--- Exporter/ExportUtilities.cs
+++ Exporter/ExportUtilities.cs
@@ -1,27 +1,32 @@
-﻿using Autodesk.Revit.DB;
+﻿using System.Collections.Generic;
+using ASRR.Revit.Core.Elements;
+using Autodesk.Revit.DB;
 using NLog;
 
-namespace ASRR.Revit.Core
+namespace ASRR.Revit.Core.Exporter
 {
-    public class ExportUtilities
+    public static class ExportUtilities
     {
-        // todo: maak static class om png te exporten
         private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
         public static void ExportPNG(Document document, string exportPath)
         {
+            var view = Utilities.Get3dView(document);
             var opt = new ImageExportOptions
             {
                 ZoomType = ZoomFitType.FitToPage,
-                PixelSize = 128,
+                PixelSize = 600,
                 FilePath = exportPath,
                 FitDirection = FitDirectionType.Horizontal,
-                HLRandWFViewsFileType = ImageFileType.JPEGLossless,
+                HLRandWFViewsFileType = ImageFileType.PNG,
                 ImageResolution = ImageResolution.DPI_600,
+                ExportRange = ExportRange.SetOfViews,
+                ViewName = exportPath,
             };
+            var ids = new List<ElementId> { view.Id };
+            opt.SetViewsAndSheets(ids);
             document.ExportImage(opt);
-
-            Log.Info("Exporting PNG");
+            Log.Info($"Exporting PNG to: {exportPath}");
         }
     }
 }
