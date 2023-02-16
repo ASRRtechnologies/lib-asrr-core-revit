a659bb0a22bfe848e8a44e70715d246057cf2f49
Amar97 <amar.ramdas@asrr.nl>
2023-02-16 11:24:34 +0100

    Update submodules

---
+++ Exporter/IFC/Service/IFCExporter.cs
@@ -0,0 +1,22 @@
+
+using ASRR.Core.File.Service;
+using Autodesk.Revit.DB;
+
+namespace ASRR.Revit.Core.Exporter.IFC.Service
+{
+    public class IFCExporter: IExporter
+    {
+        public void Export(Document document, string exportFolder, string exportFileName)
+        {
+            FileValidator.EnsureFolderExists(exportFolder);
+            
+            var ifcExportOptions = new IFCExportOptions();
+            ifcExportOptions.FileVersion = IFCVersion.IFC4;
+            ifcExportOptions.ExportBaseQuantities = true;
+            ifcExportOptions.AddOption("Name", "ASRR Technologies (ASRR B.V.)");
+            
+            document.Export(exportFolder, exportFileName, ifcExportOptions);
+        }
+      
+    }
+}
