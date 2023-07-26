6d4677c77a5440f740d2be38e75238cbceb873df
Amar97 <amar.ramdas@asrr.nl>
2023-07-26 23:16:58 +0200

    Add ifc to exporter and file uploader

--- Exporter/IFC/Service/IFCExporter.cs
+++ Exporter/IFC/Service/IFCExporter.cs
@@ -1,21 +1,27 @@
 
 using ASRR.Core.FileManagement;
+using ASRR.Revit.Core.Warnings;
 using Autodesk.Revit.DB;
 
 namespace ASRR.Revit.Core.Exporter.IFC.Service
 {
-    public class IFCExporter : IExporter
+    public class IfcExporter : IExporter
     {
         public void Export(Document document, string exportFolder, string exportFileName)
         {
             FileValidator.EnsureFolderExists(exportFolder);
-
             var ifcExportOptions = new IFCExportOptions();
             ifcExportOptions.FileVersion = IFCVersion.IFC4;
             ifcExportOptions.ExportBaseQuantities = true;
             ifcExportOptions.AddOption("Name", "ASRR Technologies (ASRR B.V.)");
 
-            document.Export(exportFolder, exportFileName, ifcExportOptions);
+            using (var transaction = WarningDiscardFailuresPreprocessor.GetTransaction(document))
+            {
+
+                transaction.Start("Exporting IFC");
+                document.Export(exportFolder, exportFileName, ifcExportOptions);
+                transaction.RollBack();
+            }
         }
 
     }
