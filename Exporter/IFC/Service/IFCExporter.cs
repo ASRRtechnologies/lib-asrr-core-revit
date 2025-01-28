bccb41a50e9f01b688ab2fac7e3963ca04f29a7d
Vanishaov <V.OedayrajsinghVarma@student.tudelft.nl>
2025-01-28 12:33:49 +0100

    feat(ifc-export): optimize ifc export configuration
    Refs HPR-403

--- Exporter/IFC/Service/IFCExporter.cs
+++ Exporter/IFC/Service/IFCExporter.cs
@@ -1,4 +1,3 @@
-
 using ASRR.Core.FileManagement;
 using ASRR.Revit.Core.Warnings;
 using Autodesk.Revit.DB;
@@ -13,16 +12,37 @@ namespace ASRR.Revit.Core.Exporter.IFC.Service
             var ifcExportOptions = new IFCExportOptions();
             ifcExportOptions.FileVersion = IFCVersion.IFC4;
             ifcExportOptions.ExportBaseQuantities = true;
+            ifcExportOptions.SpaceBoundaryLevel = 0;
             ifcExportOptions.AddOption("Name", "ASRR Technologies (ASRR B.V.)");
-
+            
             using (var transaction = WarningDiscardFailuresPreprocessor.GetTransaction(document))
             {
-
                 transaction.Start("Exporting IFC");
                 document.Export(exportFolder, exportFileName, ifcExportOptions);
                 transaction.RollBack();
             }
         }
 
+        public void Export(Document document, string exportFolder, string exportFileName,
+            BIM.IFC.Export.UI.IFCExportConfiguration ifcExportConfiguration, ElementId viewId = null)
+        {
+            FileValidator.EnsureFolderExists(exportFolder);
+            var ifcExportOptions = new IFCExportOptions();
+            if (ifcExportConfiguration == null)
+            {
+                ifcExportOptions.FileVersion = IFCVersion.IFC4;
+                ifcExportOptions.ExportBaseQuantities = true;
+                ifcExportOptions.SpaceBoundaryLevel = 0;
+                ifcExportOptions.FilterViewId = viewId;
+                ifcExportOptions.AddOption("Name", "ASRR Technologies (ASRR B.V.)");
+            }
+            else ifcExportConfiguration.UpdateOptions(ifcExportOptions, viewId);
+            using (var transaction = WarningDiscardFailuresPreprocessor.GetTransaction(document))
+            {
+                transaction.Start("Exporting IFC");
+                document.Export(exportFolder, exportFileName, ifcExportOptions);
+                transaction.RollBack();
+            }
+        }
     }
 }
